7ee1ed52d4f1a07d2ed5f2fbaa59254e3480dc23
shadik <shadikmahamudsohan@gmail.com>
2022-08-19 19:35:30 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Layout from './components/shared/Layout';
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
+    <Layout>
       <h1 className='text-red-500 text-3xl'>Hello world</h1>
       <button class="btn">Get Started</button>
-    </div>
+    </Layout>
+    // </div>
   );
 }
 
