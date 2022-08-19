523aea6888fa3d5390df0361b7134444fb3d74cb
shadik <shadikmahamudsohan@gmail.com>
2022-08-19 19:21:22 +0600

    tailwind and daisy-ui added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1 className='text-red-500 text-3xl'>Hello world</h1>
+      <button class="btn">Get Started</button>
     </div>
   );
 }
